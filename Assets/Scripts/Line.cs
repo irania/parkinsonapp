1bb2708ef3969a47b82edfb6edba8fe9b7e282dc
Irania <atefeh.ir72@gmail.com>
2023-01-15 10:43:05 +0330

    first drawing scene

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer LineRenderer;
+
+    private List<Vector2> points;
+
+    public void Awake()
+    {
+        LineRenderer = gameObject.GetComponent<LineRenderer>();
+    }
+
+    public void UpdateLine(Vector2 position)
+    {
+        if (points == null)
+        {
+            Debug.Log("i was "+ position);
+            points = new List<Vector2>();
+            SetPoint(position);
+            return;
+        }
+
+        if (Vector2.Distance(points.Last(), position) > .1f)
+        {
+            Debug.Log("i am "+ position);
+            SetPoint(position);
+        }
+    }
+    
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+        Debug.Log("i add "+ point+" "+points[points.Count-1]);
+        LineRenderer.positionCount = points.Count;
+        LineRenderer.SetPosition(points.Count()-1,point);
+    }
+}
---
+++ Assets/Scripts/LineGenerator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineGenerator : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    private Line activeLine;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newLine = Instantiate(linePrefab);
+            activeLine = newLine.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+        
+    }
+}
