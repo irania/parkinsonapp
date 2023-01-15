64e78de1249c54ece673073b0c85dbe56846e49a
Irania <atefeh.ir72@gmail.com>
2023-01-15 12:07:08 +0330

    main menu added

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    public void LoadHandwritingScene()
+    {
+        Application.LoadLevel ("DrawingScene");
+    }
+}
