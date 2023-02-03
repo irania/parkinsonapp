2373010f602e58cedfdd00519cfdc0371262a882
Irania <atefeh.ir72@gmail.com>
2023-02-03 12:10:27 +0330

    take snapshot

--- Assets/Scripts/Tasks/DrawingManager.cs
+++ Assets/Scripts/Tasks/DrawingManager.cs
@@ -74,24 +74,17 @@ public class DrawingManager : MonoBehaviour
     {
         patternIndex++;
         StartCoroutine(GoNext());
-        
-        
+
+
     }
 
     IEnumerator GoNext()
     {
-        var fileName = Application.persistentDataPath+"dr" + patternIndex +"-"+DateTime.Now.Ticks+".png";
-        try
-        {
-            ScreenCapture.CaptureScreenshot(fileName);
-        }
-        catch (Exception e)
-        {
-            debugText.text = e.Message;
-            throw;
-        }
+        var fileName = "dr" + patternIndex +"-"+DateTime.Now.Ticks+".png";
+        StartCoroutine(TakeScreenShotAndSave(fileName));
         yield return new WaitForSeconds(2);
-        SendDataManager.Instance.SendFile(fileName);
+        //SendDataManager.Instance.SendFile(Application.persistentDataPath+"/"+fileName);
+        debugText.text = Application.persistentDataPath + "/" + fileName;
         if (patternIndex== Patterns.Count)
         {
             Application.LoadLevel (0);
@@ -105,6 +98,24 @@ public class DrawingManager : MonoBehaviour
             PatternCanvas.SetActive(true);
         }
 
+    }
+
+    private IEnumerator TakeScreenShotAndSave(string filename)
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenshot.Apply();
+        
+        string path = Application.persistentDataPath + "/" + filename;
+        System.IO.File.WriteAllBytes(path, screenshot.EncodeToPNG());
+
+        // Save the screenshot to the device's photo gallery
+        AndroidJavaClass classUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject objActivity = classUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaClass classMedia = new AndroidJavaClass("android.media.MediaScannerConnection");
+        classMedia.CallStatic("scanFile", objActivity, new string[] { path }, null, new AndroidJavaProxy("android.media.MediaScannerConnection.OnScanCompletedListener"));
+        
     }
 
     public void CleanPage()
